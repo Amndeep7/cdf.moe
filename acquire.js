508ceb596e7b537c3282940cda0dace1e4612009
Amndeep Singh Mann <amndeep.dev@gmail.com>
2018-12-12 18:45:37 -0500

    Everything

---
+++ acquire.js
@@ -0,0 +1,108 @@
+(async () => {
+	try {
+		require("dotenv").config();
+
+		if (!process.argv[2]) {
+			console.log(process.argv);
+			throw "no thread argument";
+		}
+
+		const threadname = process.argv[2];
+
+		const snoowrap = require("snoowrap");
+
+		const r = new snoowrap({
+			"userAgent": process.env.REDDIT_USERAGENT,
+			"clientId": process.env.REDDIT_CLIENTID,
+			"clientSecret": process.env.REDDIT_CLIENTSECRET,
+			"refreshToken": process.env.REDDIT_REFRESHTOKEN,
+		});
+
+		r.config({
+			"continueAfterRatelimitError": true,
+			"requestDelay": 1050,
+		});
+
+		const thread = await r.getSubmission(threadname).expandReplies();
+		// console.log(thread);
+		console.log("got thread - num comments:", thread.num_comments, thread.comments.length);
+
+		const { Pool } = require("pg");
+		const pool = new Pool({
+			"user": process.env.DB_USER,
+			"password": process.env.DB_PASSWORD,
+			"host": process.env.DB_HOST,
+			"port": process.env.DB_PORT,
+			"database": process.env.DB_DATABASE,
+		});
+
+		const res = await pool.query("Select $1::text as message", ["Hello World!"]);
+		console.log(res.rows[0].message);
+
+		const tablecreationthreads = await pool.query(`
+			create table if not exists threads(
+				id serial primary key,
+				short_id char (6) not null,
+				long_id char (9) not null unique 
+			);`);
+		const tablecreationcomments = await pool.query(`
+			create table if not exists comments(
+				id serial primary key,
+				name char (10) not null unique,
+				link_id char (9) not null,
+				parent_id varchar (10) not null,
+				author varchar (20) not null,
+				depth int not null,
+				score int not null,
+				gid_1 int not null,
+				gid_2 int not null,
+				gid_3 int not null,
+				body varchar (10000) not null,
+				created_utc timestamptz not null,
+				edited timestamptz
+			);`);
+		console.log("table creations", tablecreationthreads, tablecreationcomments);
+
+		const insertthreadcommand = `
+			insert into threads (short_id, long_id) values ($1, $2) on conflict (long_id) do nothing;
+			`;
+		const insertthreadvalues = [thread.id, thread.name];
+		await pool.query(insertthreadcommand, insertthreadvalues);
+
+		let tobeinsert = thread.comments.length;
+		let completedinsert = 0;
+		const recursive_insert = (c) => {
+			console.log(c.name, c.replies.length);
+			tobeinsert += c.replies ? c.replies.length : 0;
+			const insertcommand = `
+				insert into comments (name, link_id, parent_id, author, depth, score, gid_1, gid_2, gid_3, body, created_utc, edited)
+				values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, to_timestamp($11) at time zone 'utc', to_timestamp($12) at time zone 'utc')
+				on conflict (name)
+				do update set (score, gid_1, gid_2, gid_3, body, edited) = ($6, $7, $8, $9, $10, to_timestamp($12) at time zone 'utc')
+				where comments.name = $1;`;
+			const insertvalues = [c.name, c.link_id, c.parent_id, c.author.name, c.depth, c.score, c.gildings.gid_1, c.gildings.gid_2, c.gildings.gid_3, c.body, c.created_utc, c.edited ? c.edited : null];
+			pool.query(insertcommand, insertvalues).then(() => {
+				completedinsert++;
+			});
+			c.replies.forEach((comment) => recursive_insert(comment));
+		};
+		thread.comments.forEach(recursive_insert);
+
+		await (() => new Promise((resolve, reject) => {
+			const wait = () => {
+				if (completedinsert === tobeinsert) {
+					resolve();
+				} else {
+					console.log(completedinsert, tobeinsert);
+					setTimeout(wait, 100);
+				}
+			};
+			wait();
+		}))();
+
+		await pool.end();
+
+	} catch (e) {
+		console.log("err", e);
+	}
+})();
---
+++ process.js
@@ -0,0 +1,92 @@
+(async () => {
+	try {
+		require("dotenv").config();
+
+		if (!process.argv[2]) {
+			console.log(process.argv);
+			throw "no thread argument";
+		}
+
+		if (!process.argv[3]) {
+			console.log(process.argv);
+			throw "need at least one graph type";
+		}
+
+		const threadname = process.argv[2];
+		const graphs = process.argv.slice(3);
+
+		const { Pool } = require("pg");
+		const pool = new Pool({
+			"user": process.env.DB_USER,
+			"password": process.env.DB_PASSWORD,
+			"host": process.env.DB_HOST,
+			"port": process.env.DB_PORT,
+			"database": process.env.DB_DATABASE,
+		});
+
+		const res = await pool.query("Select $1::text as message", ["Hello World!"]);
+		console.log(res.rows[0].message);
+
+		const d3nBar = require("d3node-barchart");
+		const d3nLine = require("d3node-linechart");
+		const output = require("d3node-output");
+
+		const createGraph = (name, title, type, data, constructorArgs={}) => {
+			const bar = type({...{
+				"data": data,
+				"container": `<div id="container"><h2>${title}</h2><div id="chart"></div></div>`,
+				"width": 1280,
+				"height": 670,
+			}, ...constructorArgs});
+			// console.log(bar.svgString());
+
+			output(name, bar, { "width": 1280, "height": 720 });
+		};
+
+		for (let i = 0; i < graphs.length; i++) {
+			switch(graphs[i]) {
+			case "commentsperday":
+				const commentsperday = await pool.query(`select count(*)::int as value, to_char(date(created_utc), 'Day') as key from comments inner join threads on link_id = long_id where short_id='${threadname}' group by date(created_utc) order by date(created_utc);`);
+				commentsperday.rows[commentsperday.rows.length-1].key = "Next Friday";
+				console.log(commentsperday.rows);
+				createGraph(`commentsperday_${threadname}`, "Comments per day", d3nBar, commentsperday.rows);
+				break;
+			case "commentsperhour":
+				const commentsperhour = await pool.query(`select count(*)::int as value, extract(hour from created_utc)::int as key from comments inner join threads on link_id = long_id where short_id='${threadname}' group by extract(hour from created_utc) order by key;`);
+				console.log(commentsperhour.rows);
+				createGraph(`commentsperhour_${threadname}`, "Cumulative sum of comments per hour", d3nBar, commentsperhour.rows);
+				break;
+			case "commentsperuser":
+				const commentsperuser = await pool.query(`select row_number() over (order by commentcount desc) as key, commentcount::int as value from (select count(*) as commentcount from comments inner join threads on link_id = long_id where short_id='${threadname}' group by author) as temp;`);
+				commentsperuser.rows = commentsperuser.rows.map((row) => { return { "key": parseInt(row.key), "value": row.value } });
+				console.log(commentsperuser.rows);
+				createGraph(`commentsperuser_${threadname}`, "Number of comments per (anonymized) user", d3nBar, commentsperuser.rows);
+				break;
+			case "commentsperuserhistogram":
+				const commentsperuserhistogram = await pool.query(`with temp1 as (select count(*) as commentcount from comments inner join threads on link_id = long_id where short_id='${threadname}' group by author) select width_bucket(commentcount, min, max+1, 10) as buckets, int8range(min(commentcount), max(commentcount), '[]') as key, count(*)::int as value from temp1, (select min(commentcount), max(commentcount) from temp1) as temp2 group by buckets order by buckets;`);
+				console.log(commentsperuserhistogram.rows);
+				createGraph(`commentsperuserhistogram_${threadname}`, "Histogram of number of comments per user", d3nBar, commentsperuserhistogram.rows);
+				break;
+			case "commentsperuserdecile":
+				const commentsperuserdecile = await pool.query(`select ntile, avg(commentcount)::double precision as value, min(commentcount), max(commentcount), int8range(min(commentcount), max(commentcount), '[]') as key from (select commentcount, ntile(10) over (order by commentcount) as ntile from (select count(*) as commentcount from comments inner join threads on link_id = long_id where short_id='${threadname}' group by author) as temp1) as temp2 group by ntile order by ntile;`);
+				console.log(commentsperuserdecile.rows);
+				createGraph(`commentsperuserdecile_${threadname}`, "Decile chart of number of comments per user (showcasing the average for each decile)", d3nBar, commentsperuserdecile.rows);
+				break;
+			case "parentsvschildrenperhoureachday":
+				const parentsvschildrenperhoureachday = await pool.query(`select to_char(created_utc, 'YYYY-MM-DD-HH24') as date, sum(case when link_id=parent_id then 1 else 0 end)::int as parents, sum(case when link_id!=parent_id then 1 else 0 end)::int as children from comments inner join threads on link_id = long_id where short_id='${threadname}' group by date order by date;`);
+				const pvc = [parentsvschildrenperhoureachday.rows.map(row => ({"key": new Date(...row.date.split("-")), "value": row.parents})), parentsvschildrenperhoureachday.rows.map((row) => ({"key": new Date(...row.date.split("-")), "value": row.children}))];
+				pvc["keys"] = parentsvschildrenperhoureachday.rows.map((row) => new Date(...row.date.split("-")));
+				console.log(pvc);
+				createGraph(`parentsvschildrenperhoureachday_${threadname}`, "Parents vs children comments per hour each day (ignore that the axis is in ms from the epoch)", d3nLine, pvc, {"lineColors": ['steelblue', 'darkorange']});
+				break;
+			default:
+				console.log("No handler for", graphs[i]);
+			}
+		}
+
+		await pool.end();
+
+	} catch (e) {
+		console.log("err", e);
+	}
+})();
