050f98c48a968a1d57476506a67bbfe0e434bf21
Amndeep Singh Mann <me@asm.works>
2020-05-11 04:32:42 +0000

    btw added vue at a bare minimum along with some linting, now adding some more stuff upcoming

---
+++ website/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <p><b>/u/Amndeep7</b> Crafts the Stats!</p>
+    <p><b>/u/Iron_Gland</b> Gets tagged a Lot!</p>
+    <p><b>/u/RandomRedditorWithNo</b> Leads the Search!</p>
+    <p><b>/u/lilyvess</b> Understands it All!</p>
+    <p><b>/u/AmeteurElitist</b> Spells the Best!</p>
+    <p><b>/u/thisismyanimealt</b> Codes <a href="https://friday.moe">friday.Moe</a>!</p>
+    <p><b>/u/Vinesma</b> Offers to Program!</p>
+    <p><b>/u/Lynxiusk</b> Makes the piano Forte!</p>
+    <p><b>/u/TheRiyria</b> Corners the 3x3!</p>
+    <p><b>/u/Bubaruba</b> Shills for Idols!</p>
+    <p><b>/u/ToadOnTheLonestar</b> Lewds from Texas!</p>
+    <p><b>/u/Dagonsnake</b> Goes back to School!</p>
+    <p><b>/u/Rins_husbando</b> Loves his Waifu!</p>
+    <p><b>/u/DarkAudit</b> Wins via Experience!</p>
+    <p><b>/u/max_turner</b> Knows who's Best!</p>
+    <p><b>/u/Sgtvp</b> Has the Goods!</p>
+    <p><b>/u/HelioA</b> Adores her some <del>Precure</del> Donuts!</p>
+    <p><b>/u/Shimmering-Sky</b> Illustrates the Vectors!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
