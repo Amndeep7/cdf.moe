23eb1d0d666540dfa1a88520f6f13a64eb66e8cb
Amndeep Singh Mann <me@asm.works>
2020-07-02 05:53:32 +0000

    Configuring webpack like that was unnecessary

--- website/vue.config.js
+++ website/vue.config.js
@@ -1,27 +1,4 @@
 module.exports = {
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: [
-            'vue-style-loader',
-            'css-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                implementation: require('sass'),
-                prependData: "@import '@/styles/variables.scss';",
-                sassOptions: {
-                  fiber: require('fibers'),
-                },
-              },
-            },
-          ],
-        },
-      ],
-    },
-  },
   transpileDependencies: [
     'vuetify',
   ],
