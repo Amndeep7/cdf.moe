66d6f60e0223815a6fc119b54314f5487f6cf9fa
Amndeep Singh Mann <me@asm.works>
2020-08-10 06:15:53 +0000

    Improved the svg for the fish icon a bit, standardized the font for it, and found a webpack plugin that generates all the jillion pngs I need for when svg don't work

--- website/vue.config.js
+++ website/vue.config.js
@@ -1,3 +1,5 @@
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
 module.exports = {
   chainWebpack: (config) => {
     config
@@ -5,8 +7,17 @@ module.exports = {
     .tap((args) => {
       // eslint-disable-next-line no-param-reassign
       args[0].title = 'CDF.MOE';
+      // eslint-disable-next-line no-param-reassign
+      args[0].favicon = './public/favicon.svg';
       return args;
     });
+  },
+  configureWebpack: {
+    plugins: [
+      new FaviconsWebpackPlugin({
+        logo: './public/favicon.svg',
+      }),
+    ],
   },
   transpileDependencies: [
     'vuetify',
